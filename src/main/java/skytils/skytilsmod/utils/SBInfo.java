c3298fc24ee9776adadf036687cf8259d9fbf11d
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2021-02-10 09:17:53 -0500

    Update Locraw Util

--- src/main/java/skytils/skytilsmod/utils/SBInfo.java
+++ src/main/java/skytils/skytilsmod/utils/SBInfo.java
@@ -3,12 +3,15 @@ package skytils.skytilsmod.utils;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.inventory.GuiChest;
+import net.minecraft.inventory.ContainerChest;
 import net.minecraft.scoreboard.Score;
 import net.minecraft.scoreboard.ScoreObjective;
 import net.minecraft.scoreboard.ScorePlayerTeam;
 import net.minecraft.scoreboard.Scoreboard;
 import net.minecraft.util.StringUtils;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -43,11 +46,29 @@ public class SBInfo {
     public String mode = "";
 
     public Date currentTimeDate = null;
-    private long lastLocRaw = -1;
-    private JsonObject locraw = null;
+
+    public String lastOpenContainerName = null;
 
     public static SBInfo getInstance() {
         return INSTANCE;
+    }
+
+    private long lastManualLocRaw = -1;
+    private long lastLocRaw = -1;
+    private long joinedWorld = -1;
+    private JsonObject locraw = null;
+
+    @SubscribeEvent
+    public void onGuiOpen(GuiOpenEvent event) {
+        if (!Utils.inSkyblock) return;
+
+        if(event.gui instanceof GuiChest) {
+            GuiChest chest = (GuiChest) event.gui;
+            ContainerChest container = (ContainerChest) chest.inventorySlots;
+            String containerName = container.getLowerChestInventory().getDisplayName().getUnformattedText();
+
+            lastOpenContainerName = containerName;
+        }
     }
 
     @SubscribeEvent
@@ -55,39 +76,54 @@ public class SBInfo {
         lastLocRaw = -1;
         locraw = null;
         mode = null;
+        joinedWorld = System.currentTimeMillis();
+        lastOpenContainerName = null;
     }
 
-    @SubscribeEvent(priority = EventPriority.HIGHEST, receiveCanceled = true)
+    private static final Pattern JSON_BRACKET_PATTERN = Pattern.compile("\\{.+}");
+
+    public void onSendChatMessage(String msg) {
+        if(msg.trim().startsWith("/locraw") || msg.trim().startsWith("/locraw ")) {
+            lastManualLocRaw = System.currentTimeMillis();
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOW, receiveCanceled = true)
     public void onChatMessage(ClientChatReceivedEvent event) {
-        if (event.message.getUnformattedText().startsWith("{")) {
+        Matcher matcher = JSON_BRACKET_PATTERN.matcher(event.message.getUnformattedText());
+        if(matcher.find()) {
             try {
-                JsonObject obj = new Gson().fromJson(event.message.getUnformattedText(), JsonObject.class);
-                if (obj.has("server")) {
-                    if (!Skytils.usingNEU && System.currentTimeMillis() - lastLocRaw < 5000) event.setCanceled(true);
-                    if (obj.has("gametype") && obj.has("mode") && obj.has("map")) {
+                JsonObject obj = new Gson().fromJson(matcher.group(), JsonObject.class);
+                if(obj.has("server")) {
+                    if(System.currentTimeMillis() - lastManualLocRaw > 5000) event.setCanceled(true);
+                    if(obj.has("gametype") && obj.has("mode") && obj.has("map")) {
                         locraw = obj;
                         mode = locraw.get("mode").getAsString();
                     }
                 }
-            } catch (Exception e) {
+            } catch(Exception e) {
                 e.printStackTrace();
             }
         }
     }
 
     public String getLocation() {
-        if (mode == null) {
+        if(mode == null) {
             return null;
         }
         return mode;
     }
 
     @SubscribeEvent
-    public void tick(TickEvent.ClientTickEvent.ClientTickEvent event) {
-        if (event.phase != TickEvent.Phase.START || Minecraft.getMinecraft().thePlayer == null || Minecraft.getMinecraft().theWorld == null) return;
-        if (Minecraft.getMinecraft().theWorld != null &&
-                locraw == null && (System.currentTimeMillis() - lastLocRaw) > 20000 &&
-                Utils.isOnHypixel()) {
+    public void onTick(TickEvent.ClientTickEvent event) {
+        if (event.phase != TickEvent.Phase.START || Minecraft.getMinecraft().thePlayer == null || Minecraft.getMinecraft().theWorld == null || !Utils.inSkyblock) return;
+        long currentTime = System.currentTimeMillis();
+
+        if(Minecraft.getMinecraft().thePlayer != null &&
+                Minecraft.getMinecraft().theWorld != null &&
+                locraw == null &&
+                (currentTime - joinedWorld) > 1000 &&
+                (currentTime - lastLocRaw) > 15000) {
             lastLocRaw = System.currentTimeMillis();
             Skytils.sendMessageQueue.add("/locraw");
         }
@@ -100,39 +136,38 @@ public class SBInfo {
             List<Score> scores = new ArrayList<>(scoreboard.getSortedScores(sidebarObjective));
 
             List<String> lines = new ArrayList<>();
-            for (int i = scores.size() - 1; i >= 0; i--) {
+            for(int i=scores.size()-1; i>=0; i--) {
                 Score score = scores.get(i);
                 ScorePlayerTeam scoreplayerteam1 = scoreboard.getPlayersTeam(score.getPlayerName());
                 String line = ScorePlayerTeam.formatPlayerName(scoreplayerteam1, score.getPlayerName());
                 line = StringUtils.stripControlCodes(line);
                 lines.add(line);
             }
-            if (lines.size() >= 5) {
+            if(lines.size() >= 5) {
                 date = StringUtils.stripControlCodes(lines.get(1)).trim();
                 //§74:40am
                 Matcher matcher = timePattern.matcher(lines.get(2));
-                if (matcher.find()) {
+                if(matcher.find()) {
                     time = StringUtils.stripControlCodes(matcher.group()).trim();
                     try {
                         String timeSpace = time.replace("am", " am").replace("pm", " pm");
                         SimpleDateFormat parseFormat = new SimpleDateFormat("hh:mm a");
                         currentTimeDate = parseFormat.parse(timeSpace);
-                    } catch (ParseException e) {
-                    }
+                    } catch (ParseException e) {}
                 }
                 location = StringUtils.stripControlCodes(lines.get(3)).replaceAll("[^A-Za-z0-9() ]", "").trim();
             }
             objective = null;
 
             boolean objTextLast = false;
-            for (String line : lines) {
-                if (objTextLast) {
+            for(String line : lines) {
+                if(objTextLast) {
                     objective = line;
                 }
 
                 objTextLast = line.equals("Objective");
             }
-        } catch (Exception e) {
+        } catch(Exception e) {
             e.printStackTrace();
         }
     }
