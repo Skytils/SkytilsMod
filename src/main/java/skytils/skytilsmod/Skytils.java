a62acee75727fcb3cecb0e47f824521869be854c
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2021-02-23 18:42:44 -0500

    0.0.8-pre2

--- src/main/java/skytils/skytilsmod/Skytils.java
+++ src/main/java/skytils/skytilsmod/Skytils.java
@@ -41,7 +41,7 @@ import java.util.Map;
 public class Skytils {
     public static final String MODID = "skytils";
     public static final String MOD_NAME = "Skytils";
-    public static final String VERSION = "0.0.8-pre1";
+    public static final String VERSION = "0.0.8-pre2";
     public static final Minecraft mc = Minecraft.getMinecraft();
 
     public static Config config = new Config();
