96185046cef43d3894c615ee106e972e81c543dc
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2021-03-01 14:28:46 -0500

    Hide Jerry rune

--- src/main/java/skytils/skytilsmod/core/Config.java
+++ src/main/java/skytils/skytilsmod/core/Config.java
@@ -540,6 +540,15 @@ public class Config extends Vigilant {
 
     @Property(
             type = PropertyType.SWITCH,
+            name = "Hide Jerry Rune",
+            description = "Prevents the game from rendering the items spawned by the Jerry rune.",
+            category = "Miscellaneous",
+            subcategory = "Quality of Life"
+    )
+    public boolean hideJerryRune = false;
+
+    @Property(
+            type = PropertyType.SWITCH,
             name = "Hide Lightning",
             description = "Prevents all lightning from rendering.",
             category = "Miscellaneous",
