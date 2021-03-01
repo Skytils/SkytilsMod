64a373dfe8a50dbeca605767a6b7446150638323
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2021-03-01 17:40:52 -0500

    Update Config.java

--- src/main/java/skytils/skytilsmod/core/Config.java
+++ src/main/java/skytils/skytilsmod/core/Config.java
@@ -131,15 +131,6 @@ public class Config extends Vigilant {
 
     @Property(
             type = PropertyType.SWITCH,
-            name = "Revive Stone Names",
-            description = "Shows names next to the heads on the Revive Stone menu.",
-            category = "Dungeons",
-            subcategory = "Quality of Life"
-    )
-    public boolean reviveStoneNames = false;
-
-    @Property(
-            type = PropertyType.SWITCH,
             name = "Spirit Leap Names",
             description = "Shows names next to the heads on the Spirit Leap menu.",
             category = "Dungeons",
