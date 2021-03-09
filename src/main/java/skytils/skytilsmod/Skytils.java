f2983a06d7c97a08041453ca6c4b2c89264716c5
Sychic <47618543+Sychic@users.noreply.github.com>
2021-03-09 17:56:52 -0500

    0.1.2-pre1

--- src/main/java/skytils/skytilsmod/Skytils.java
+++ src/main/java/skytils/skytilsmod/Skytils.java
@@ -42,7 +42,7 @@ import java.util.Map;
 public class Skytils {
     public static final String MODID = "skytils";
     public static final String MOD_NAME = "Skytils";
-    public static final String VERSION = "0.1.1";
+    public static final String VERSION = "0.1.2-pre1";
     public static final Minecraft mc = Minecraft.getMinecraft();
 
     public static Config config = new Config();
