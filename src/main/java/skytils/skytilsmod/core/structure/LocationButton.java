d8a5ee961403a2f5143480ce73638eaf22053d53
Sychic <47618543+Sychic@users.noreply.github.com>
2021-03-26 11:53:29 -0400

    add background to gui elements while editing locations

--- src/main/java/skytils/skytilsmod/core/structure/LocationButton.java
+++ src/main/java/skytils/skytilsmod/core/structure/LocationButton.java
@@ -3,16 +3,25 @@ package skytils.skytilsmod.core.structure;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.audio.SoundHandler;
 import net.minecraft.client.gui.GuiButton;
+import skytils.skytilsmod.utils.RenderUtil;
+
+import java.awt.*;
 
 public class LocationButton extends GuiButton {
     private float x;
     private float y;
+    private float x2;
+    private float y2;
     private double scale;
     public GuiElement element;
 
     public LocationButton(GuiElement element) {
         super(-1, 0, 0, null);
         this.element = element;
+        this.x = this.element.getActualX() - 5;
+        this.y = this.element.getActualY() - 5;
+        this.x2 = this.x + this.element.getWidth() + 5;
+        this.y2 = this.y + this.element.getHeight() + 5;
     }
 
     public LocationButton(int buttonId, GuiElement element) {
@@ -20,8 +29,21 @@ public class LocationButton extends GuiButton {
         this.element = element;
     }
 
+    private void refreshLocations() {
+        this.x = this.element.getActualX() - 2;
+        this.y = this.element.getActualY() - 2;
+        this.x2 = this.x + this.element.getWidth() + 4;
+        this.y2 = this.y + this.element.getHeight() + 4;
+    }
+
     @Override
     public void drawButton(Minecraft mc, int mouseX, int mouseY) {
+        Color c = new Color(255,255,255,40);
+        if (hovered) {
+            c = new Color(255, 255, 255, 100);
+        }
+        refreshLocations();
+        RenderUtil.drawRect(x, y, x2, y2, c.getRGB());
         this.element.demoRender();
         hovered = mouseX >= this.element.getActualX() && mouseY >= this.getElement().getActualY() && mouseX < this.getElement().getActualX() + this.getElement().getWidth() && mouseY < this.getElement().getActualY() + this.getElement().getHeight();
     }
--- src/main/java/skytils/skytilsmod/utils/RenderUtil.java
+++ src/main/java/skytils/skytilsmod/utils/RenderUtil.java
@@ -450,4 +450,47 @@ public class RenderUtil {
             GlStateManager.disableAlpha();
         }
     }
+
+    /**
+     * Taken from SkyblockAddons under MIT License
+     * Modified
+     * https://github.com/BiscuitDevelopment/SkyblockAddons/blob/master/LICENSE
+     * @author BiscuitDevelopment
+     *
+     * Draws a solid color rectangle with the specified coordinates and color (ARGB format). Args: x1, y1, x2, y2, color
+     */
+    public static void drawRect(double left, double top, double right, double bottom, int color) {
+        if (left < right) {
+            double i = left;
+            left = right;
+            right = i;
+        }
+
+        if (top < bottom) {
+            double j = top;
+            top = bottom;
+            bottom = j;
+        }
+
+        float f3 = (float) (color >> 24 & 255) / 255.0F;
+        float f = (float) (color >> 16 & 255) / 255.0F;
+        float f1 = (float) (color >> 8 & 255) / 255.0F;
+        float f2 = (float) (color & 255) / 255.0F;
+        GlStateManager.color(f, f1, f2, f3);
+
+        Tessellator tessellator = Tessellator.getInstance();
+        WorldRenderer worldrenderer = tessellator.getWorldRenderer();
+        GlStateManager.enableBlend();
+        GlStateManager.disableTexture2D();
+        GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+
+        worldrenderer.begin(7, DefaultVertexFormats.POSITION);
+        worldrenderer.pos(left, bottom, 0.0D).endVertex();
+        worldrenderer.pos(right, bottom, 0.0D).endVertex();
+        worldrenderer.pos(right, top, 0.0D).endVertex();
+        worldrenderer.pos(left, top, 0.0D).endVertex();
+        tessellator.draw();
+        GlStateManager.enableTexture2D();
+        GlStateManager.disableBlend();
+    }
 }
