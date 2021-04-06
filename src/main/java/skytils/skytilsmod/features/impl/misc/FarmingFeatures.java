35874632b49fc036dea90c47a33a416510face33
Cuddy Steadman <cchsteadman@gmail.com>
2021-04-05 20:32:52 -0400

    fix

--- src/main/java/skytils/skytilsmod/features/impl/misc/FarmingFeatures.java
+++ src/main/java/skytils/skytilsmod/features/impl/misc/FarmingFeatures.java
@@ -66,7 +66,7 @@ public class FarmingFeatures {
 
             if ((heldItem.getItem() instanceof ItemHoe || heldItem.getItem() instanceof ItemAxe) && farmBlocks.contains(block)) {
                 event.setCanceled(true);
-                if (Skytils.config.stopFarmingMsgs) {
+                if (!Skytils.config.stopFarmingMsgs) {
                     if (System.currentTimeMillis() - lastNotifyBreakTime > 10000) {
                         lastNotifyBreakTime = System.currentTimeMillis();
                         p.playSound("note.bass", 1, 0.5f);
