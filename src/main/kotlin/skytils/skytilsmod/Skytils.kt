12569a98b69512d0abe8fd4ff242114f5dc66827
Sychic <47618543+Sychic@users.noreply.github.com>
2021-07-14 10:25:57 -0400

    1.0-pre17

--- src/main/kotlin/skytils/skytilsmod/Skytils.kt
+++ src/main/kotlin/skytils/skytilsmod/Skytils.kt
@@ -98,7 +98,7 @@ class Skytils {
     companion object {
         const val MODID = "skytils"
         const val MOD_NAME = "Skytils"
-        const val VERSION = "1.0-pre16"
+        const val VERSION = "1.0-pre17"
 
         @JvmField
         val gson: Gson = GsonBuilder().setPrettyPrinting().create()
