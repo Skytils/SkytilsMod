0aada086dbd3fd7268c01379ac76400a8abba8a2
DeDiamondPro <67508414+DeDiamondPro@users.noreply.github.com>
2021-07-15 18:39:56 +0200

    fix a crash because inSkyblock is true when not connected to server

--- src/main/kotlin/skytils/skytilsmod/Skytils.kt
+++ src/main/kotlin/skytils/skytilsmod/Skytils.kt
@@ -41,6 +41,7 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.fml.common.gameevent.TickEvent
+import net.minecraftforge.fml.common.network.FMLNetworkEvent
 import org.lwjgl.input.Mouse
 import skytils.skytilsmod.commands.*
 import skytils.skytilsmod.commands.stats.impl.CataCommand
@@ -309,6 +310,13 @@ class Skytils {
         }
 
         ticks++
+    }
+
+    @SubscribeEvent
+    fun onDisconnect(event: FMLNetworkEvent.ClientDisconnectionFromServerEvent){
+        Utils.isOnHypixel = false
+        Utils.inSkyblock = false
+        Utils.inDungeons = false
     }
 
     @SubscribeEvent
