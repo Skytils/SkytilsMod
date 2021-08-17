081dc210df4eb19eb8f18355a9feef22024d4e26
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2021-08-17 19:39:30 -0400

    Remove call to super on skytils security manager

--- src/main/java/skytils/skytilsmod/tweaker/SkytilsSecurityManager.java
+++ src/main/java/skytils/skytilsmod/tweaker/SkytilsSecurityManager.java
@@ -27,16 +27,24 @@ public class SkytilsSecurityManager extends FMLSecurityManager {
     @Override
     public void checkPermission(Permission perm) {
         String permName = perm.getName() != null ? perm.getName() : "missing";
-        if (permName.startsWith("exitVM") && Minecraft.getMinecraft() == null) {
+        if (permName.startsWith("exitVM")) {
             Class<?>[] classContexts = getClassContext();
             String callingClass = classContexts.length > 3 ? classContexts[4].getName() : "none";
-            // Skytils: allow the LaunchWrapper to close the game
-            if (callingClass.equals("net.minecraft.launchwrapper.Launch")) {
+            String callingParent = classContexts.length > 4 ? classContexts[5].getName() : "none";
+            if (callingClass.equals("net.minecraft.launchwrapper.Launch") && callingParent.equals("net.minecraft.launchwrapper.Launch")) {
                 return;
+            }
+            // FML is allowed to call system exit and the Minecraft applet (from the quit button)
+            if (!(callingClass.startsWith("net.minecraftforge.fml.")
+                    || "net.minecraft.server.dedicated.ServerHangWatchdog$1".equals(callingClass)
+                    || "net.minecraft.server.dedicated.ServerHangWatchdog".equals(callingClass)
+                    || ( "net.minecraft.client.Minecraft".equals(callingClass) && "net.minecraft.client.Minecraft".equals(callingParent))
+                    || ("net.minecraft.server.dedicated.DedicatedServer".equals(callingClass) && "net.minecraft.server.MinecraftServer".equals(callingParent)))
+            ) {
+                throw new ExitTrappedException();
             }
         } else if ("setSecurityManager".equals(permName)) {
             throw new SecurityException("Cannot replace the FML (Skytils) security manager");
         }
-        super.checkPermission(perm);
     }
 }
