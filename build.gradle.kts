119e520488810eb639633abba87bc55415251ef0
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2021-09-02 21:43:04 -0400

    1.0.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ plugins {
     java
 }
 
-version = "1.0.6-pre1"
+version = "1.0.6"
 group = "skytils.skytilsmod"
 
 minecraft {
--- src/main/kotlin/skytils/skytilsmod/Skytils.kt
+++ src/main/kotlin/skytils/skytilsmod/Skytils.kt
@@ -96,7 +96,7 @@ class Skytils {
     companion object {
         const val MODID = "skytils"
         const val MOD_NAME = "Skytils"
-        const val VERSION = "1.0.6-pre1"
+        const val VERSION = "1.0.6"
 
         @JvmField
         val gson: Gson = GsonBuilder().setPrettyPrinting().create()
