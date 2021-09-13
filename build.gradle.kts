e9cbe5ab8ff8529e2d9c1de9941a39fb5e650d2c
Sychic <47618543+Sychic@users.noreply.github.com>
2021-09-13 16:44:03 -0400

    Rebase build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -82,7 +82,7 @@ dependencies {
     }
 
     shadowMe("org.apache.httpcomponents.client5:httpclient5:5.1.3")
-    shadowMe("com.github.Skytils:Hylin:a9899c8c03") {
+    shadowMe("com.github.Skytils:Hylin:2957ca8d78") {
         exclude(module = "kotlin-reflect")
         exclude(module = "kotlin-stdlib-jdk8")
         exclude(module = "kotlin-stdlib-jdk7")
