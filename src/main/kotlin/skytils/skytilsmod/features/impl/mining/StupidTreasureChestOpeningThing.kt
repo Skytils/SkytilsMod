699b2006ad1527585a9b38ffc2a07781b1c5009f
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2021-10-09 16:41:47 -0400

    increase sound testing range

--- src/main/kotlin/skytils/skytilsmod/features/impl/mining/StupidTreasureChestOpeningThing.kt
+++ src/main/kotlin/skytils/skytilsmod/features/impl/mining/StupidTreasureChestOpeningThing.kt
@@ -115,7 +115,8 @@ object StupidTreasureChestOpeningThing {
                         it.key.distanceSq(x, y, z)
                     } ?: return
 
-                    if (probable.key.distanceSq(x, y, z) < 2.5 * 2.5) {
+                    UChat.chat("$sound distance from chest ${probable.key.distanceSq(x, y, z)}")
+                    if (probable.key.distanceSq(x, y, z) < 6.9 * 6.9) {
                         probable.value.progress += if (sound == "random.orb") 1 else -1
                         UChat.chat("sound $sound, $pitch pitch, $volume volume, at $x, $y, $z")
                     }
