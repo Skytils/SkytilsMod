6b53972809151e6253934d89bc2342afeaac6c83
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2021-10-16 19:29:57 -0400

    trigger name check on spawned mobs

--- src/main/java/skytils/skytilsmod/mixins/transformers/network/MixinNetHandlerPlayClient.java
+++ src/main/java/skytils/skytilsmod/mixins/transformers/network/MixinNetHandlerPlayClient.java
@@ -19,18 +19,33 @@
 package skytils.skytilsmod.mixins.transformers.network;
 
 import net.minecraft.client.network.NetHandlerPlayClient;
+import net.minecraft.entity.DataWatcher;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.INetHandlerPlayClient;
+import net.minecraft.network.play.server.S0FPacketSpawnMob;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import skytils.skytilsmod.mixins.extensions.ExtensionEntityLivingBase;
 import skytils.skytilsmod.mixins.hooks.network.NetHandlerPlayClientHookKt;
+
+import java.util.List;
 
 @Mixin(value = NetHandlerPlayClient.class, priority = 1001)
 public abstract class MixinNetHandlerPlayClient implements INetHandlerPlayClient {
     @Inject(method = "addToSendQueue", at = @At("HEAD"), cancellable = true)
     private void onSendPacket(Packet<?> packet, CallbackInfo ci) {
         NetHandlerPlayClientHookKt.onSendPacket(packet, ci);
+    }
+
+    @Inject(method = "handleSpawnMob", at = @At("RETURN"), locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void onHandleSpawnMobTail(S0FPacketSpawnMob i, CallbackInfo ci, double d0, double d1, double d2, float f, float f1, EntityLivingBase entity, Entity[] parts, List<DataWatcher.WatchableObject> list) {
+        ((ExtensionEntityLivingBase)entity).getSkytilsHook().onNewDisplayName(
+            entity.getDataWatcher().getWatchableObjectString(2)
+        );
     }
 }
