0526603e4be27062a006bfb1d200805b4a774664
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2022-01-18 21:00:28 -0500

    average sychicer

--- src/main/java/skytils/skytilsmod/mixins/transformers/renderer/MixinLayerArmorBase.java
+++ src/main/java/skytils/skytilsmod/mixins/transformers/renderer/MixinLayerArmorBase.java
@@ -38,21 +38,25 @@ import static skytils.skytilsmod.Skytils.getMc;
 @Mixin(LayerArmorBase.class)
 public abstract class MixinLayerArmorBase<T extends ModelBase> implements LayerRenderer<EntityLivingBase> {
 
-    @Shadow @Final private RendererLivingEntity<?> renderer;
+    @Shadow
+    @Final
+    private RendererLivingEntity<?> renderer;
 
-    @Shadow private float alpha;
+    @Shadow
+    private float alpha;
 
     @Unique
     private boolean modifiedAlpha = false;
 
     @Inject(method = "doRenderLayer", at = @At("HEAD"), cancellable = true)
     private void onRenderAllArmor(EntityLivingBase entitylivingbaseIn, float p_177141_2_, float p_177141_3_, float partialTicks, float p_177141_5_, float p_177141_6_, float p_177141_7_, float scale, CallbackInfo ci) {
-        if (Config.INSTANCE.getTransparentArmorLayer() == 0 && Utils.inSkyblock && entitylivingbaseIn == getMc().thePlayer) ci.cancel();
+        if (Config.INSTANCE.getTransparentArmorLayer() == 0 && Utils.INSTANCE.getInSkyblock() && entitylivingbaseIn == getMc().thePlayer)
+            ci.cancel();
     }
 
     @Inject(method = "renderLayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemArmor;getColor(Lnet/minecraft/item/ItemStack;)I"))
     private void setAlpha(EntityLivingBase entitylivingbaseIn, float p_177182_2_, float p_177182_3_, float partialTicks, float p_177182_5_, float p_177182_6_, float p_177182_7_, float scale, int armorSlot, CallbackInfo ci) {
-        if (Utils.inSkyblock && entitylivingbaseIn == getMc().thePlayer) {
+        if (Utils.INSTANCE.getInSkyblock() && entitylivingbaseIn == getMc().thePlayer) {
             modifiedAlpha = true;
             this.alpha = Config.INSTANCE.getTransparentArmorLayer();
             GlStateManager.enableBlend();
