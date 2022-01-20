0b50b38757b4803878ffc3891a63c94ace48ba3f
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2022-01-20 09:35:49 -0500

    idk send help

--- src/main/kotlin/skytils/skytilsmod/features/impl/handlers/MayorInfo.kt
+++ src/main/kotlin/skytils/skytilsmod/features/impl/handlers/MayorInfo.kt
@@ -174,7 +174,7 @@ object MayorInfo {
                     }
                 }
             }
-            if (currentMayor == "Jerry" && chestName == "Mayor Jerry" && event.slot.slotNumber == 11 && event.slot.hasStack) {
+            if (currentMayor == "Jerry" && ((chestName == "Mayor Jerry" && event.slot.slotNumber == 11) || (chestName == "Calendar and Events" || event.slot.slotNumber == 46)) && event.slot.hasStack) {
                 val lore = ItemUtil.getItemLore(event.slot.stack)
                 if (!lore.contains("§9Perkpocalypse Perks:")) return
                 val endingIn = lore.find { it.startsWith("§7Next set of perks in") } ?: return
