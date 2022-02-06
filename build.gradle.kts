3e4c01505ac549bedd1bf5a9da88b4e9c793f062
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2022-02-06 12:04:58 -0500

    Bump Hylin

--- build.gradle.kts
+++ build.gradle.kts
@@ -82,7 +82,7 @@ dependencies {
     }
 
     shadowMe("org.apache.httpcomponents.client5:httpclient5:5.1.3")
-    shadowMe("com.github.Skytils:Hylin:89cc0fdbaf") {
+    shadowMe("com.github.Skytils:Hylin:b7c6f75f9a") {
         exclude(module = "kotlin-reflect")
         exclude(module = "kotlin-stdlib-jdk8")
         exclude(module = "kotlin-stdlib-jdk7")
