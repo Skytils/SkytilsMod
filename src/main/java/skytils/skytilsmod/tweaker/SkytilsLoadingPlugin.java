e37926d003e13ee0261a190c380b90f8e6d1c41f
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2022-02-17 09:22:27 -0500

    bump the required versions

--- src/main/java/skytils/skytilsmod/tweaker/SkytilsLoadingPlugin.java
+++ src/main/java/skytils/skytilsmod/tweaker/SkytilsLoadingPlugin.java
@@ -39,67 +39,68 @@ public class SkytilsLoadingPlugin implements IFMLLoadingPlugin {
 
     public static final String missingDependency =
             "<html><p>" +
-            "Skytils has detected a possible missing dependency<br>" +
-            "The most likely reason is Essential failed to load.<br>" +
-            "Essential might also not work in your country.<br>" +
-            "Check the Skytils Discord for any announcements, and<br>" +
-            "if there are none, ask for support." +
-            "</p></html>";
+                    "Skytils has detected a possible missing dependency<br>" +
+                    "The most likely reason is Essential failed to load.<br>" +
+                    "Wait a bit, then restart your game.<br>" +
+                    "Essential might also not work in your country.<br>" +
+                    "Check the Skytils Discord for any announcements, and<br>" +
+                    "if there are none, ask for support." +
+                    "</p></html>";
 
     public static final String kotlinErrorMessage =
-        "<html><p>" +
-        "Skytils has detected a mod with an older version of Kotlin.<br>" +
-        "The most common culprit is the ChatTriggers mod.<br>" +
-        "If you do have ChatTriggers, you can update to 1.3.2<br>" +
-        "or later to fix the issue. https://www.chattriggers.com/<br>" +
-        "In order to resolve this conflict you must<br>" +
-        "deleted the outdated mods.<br>" +
-        "If you have already done this and are still getting this error,<br>" +
-        "or need assistance, ask for support in the Discord.";
+            "<html><p>" +
+                    "Skytils has detected a mod with an older version of Kotlin.<br>" +
+                    "The most common culprit is the ChatTriggers mod.<br>" +
+                    "If you do have ChatTriggers, you can update to 1.3.2<br>" +
+                    "or later to fix the issue. https://www.chattriggers.com/<br>" +
+                    "In order to resolve this conflict you must<br>" +
+                    "deleted the outdated mods.<br>" +
+                    "If you have already done this and are still getting this error,<br>" +
+                    "or need assistance, ask for support in the Discord.";
 
     public static final String badMixinVersionMessage =
-        "<html><p>" +
-        "Skytils has detected an older version of Mixin.<br>" +
-        "Many of my features require Mixin 0.7 or later!<br>" +
-        "In order to resolve this conflict you must remove<br>" +
-        "any mods with a Mixin version below 0.7.<br>" +
-        "You can also try to rename Skytils be above other mods alphabetically<br>" +
-        "by changing Skytils.jar to !Skytils.jar<br>" +
-        "If you have already done this and are still getting this error,<br>" +
-        "ask for support in the Discord.";
+            "<html><p>" +
+                    "Skytils has detected an older version of Mixin.<br>" +
+                    "Many of my features require Mixin 0.8 or later!<br>" +
+                    "In order to resolve this conflict you must remove<br>" +
+                    "any mods with a Mixin version below 0.8.<br>" +
+                    "You can also try to rename Skytils be above other mods alphabetically<br>" +
+                    "by changing Skytils.jar to !Skytils.jar<br>" +
+                    "If you have already done this and are still getting this error,<br>" +
+                    "ask for support in the Discord.";
 
     public static final String liteloaderUserMessage =
-        "<html><p>" +
-        "Skytils has detected that you are using LiteLoader.<br>" +
-        "LiteLoader bundles an older, incompatible version of Mixin.<br>" +
-        "In order to resolve this conflict you must launch<br>" +
-        "Minecraft without LiteLoader.<br>" +
-        "If you have already done this and are still getting this error,<br>" +
-        "ask for support in the Discord." +
-        "</p></html>";
+            "<html><p>" +
+                    "Skytils has detected that you are using LiteLoader.<br>" +
+                    "LiteLoader bundles an older, incompatible version of Mixin.<br>" +
+                    "In order to resolve this conflict you must launch<br>" +
+                    "Minecraft without LiteLoader.<br>" +
+                    "If you have already done this and are still getting this error,<br>" +
+                    "ask for support in the Discord." +
+                    "</p></html>";
 
     private static final String voidChatMessage =
-        "<html><p>" +
-        "Skytils has detected that you are using VoidChat.<br>" +
-        "VoidChat breaks many of my features!<br>" +
-        "In order to resolve this conflict you must remove<br>" +
-        "VoidChat from your Minecraft mods folder.<br>" +
-        "A good alternative is Patcher at https://sk1er.club/mods/Patcher.<br>" +
-        "If you have already done this and are still getting this error,<br>" +
-        "ask for support in the Discord." +
-        "</p></html>";
+            "<html><p>" +
+                    "Skytils has detected that you are using VoidChat.<br>" +
+                    "VoidChat breaks many of my features!<br>" +
+                    "In order to resolve this conflict you must remove<br>" +
+                    "VoidChat from your Minecraft mods folder.<br>" +
+                    "A good alternative is Patcher at https://sk1er.club/mods/Patcher.<br>" +
+                    "If you have already done this and are still getting this error,<br>" +
+                    "ask for support in the Discord." +
+                    "</p></html>";
 
     private static final String betterFPSMessage =
-        "<html><p>" +
-        "Skytils has detected that you are using BetterFPS.<br>" +
-        "BetterFPS breaks my core plugins, and also breaks the game!<br>" +
-        "In order to resolve this conflict you must remove<br>" +
-        "BetterFPS from your Minecraft mods folder.<br>" +
-        "You probably will not notice a change in your FPS.<br>" +
-        "Video showcasing breaking changes: https://streamable.com/q4ip5u.<br>" +
-        "If you have already done this and are still getting this error,<br>" +
-        "ask for support in the Discord." +
-        "</p></html>";
+            "<html><p>" +
+                    "Skytils has detected that you are using BetterFPS.<br>" +
+                    "BetterFPS breaks my core plugins, and also breaks the game!<br>" +
+                    "In order to resolve this conflict you must remove<br>" +
+                    "BetterFPS from your Minecraft mods folder.<br>" +
+                    "You probably will not notice a change in your FPS.<br>" +
+                    "Video showcasing breaking changes: https://streamable.com/q4ip5u.<br>" +
+                    "If you have already done this and are still getting this error,<br>" +
+                    "ask for support in the Discord." +
+                    "</p></html>";
 
     private final SkytilsLoadingPluginKt kotlinPlugin;
 
@@ -139,6 +140,21 @@ public class SkytilsLoadingPlugin implements IFMLLoadingPlugin {
             }
         }
         kotlinPlugin = new SkytilsLoadingPluginKt();
+    }
+
+    /**
+     * Bypasses forges security manager to exit the jvm
+     */
+    public static void exit() {
+        try {
+            Class<?> clazz = Class.forName("java.lang.Shutdown");
+            Method m_exit = clazz.getDeclaredMethod("exit", int.class);
+            m_exit.setAccessible(true);
+            m_exit.invoke(null, 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 
     private boolean checkForClass(String className) {
@@ -196,31 +212,16 @@ public class SkytilsLoadingPlugin implements IFMLLoadingPlugin {
 
         Object[] options = new Object[]{discordLink, close};
         JOptionPane.showOptionDialog(
-            frame,
-            errorMessage,
-            "Skytils Error",
-            JOptionPane.DEFAULT_OPTION,
-            JOptionPane.ERROR_MESSAGE,
-            icon,
-            options,
-            options[0]
+                frame,
+                errorMessage,
+                "Skytils Error",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.ERROR_MESSAGE,
+                icon,
+                options,
+                options[0]
         );
         exit();
-    }
-
-    /**
-     * Bypasses forges security manager to exit the jvm
-     */
-    public static void exit() {
-        try {
-            Class<?> clazz = Class.forName("java.lang.Shutdown");
-            Method m_exit = clazz.getDeclaredMethod("exit", int.class);
-            m_exit.setAccessible(true);
-            m_exit.invoke(null, 0);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
     }
 
     @Override
