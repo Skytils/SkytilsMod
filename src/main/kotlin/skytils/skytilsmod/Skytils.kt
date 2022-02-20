48cf23a36c6e104f1f16a247c74d24f3c2fe2573
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2022-02-20 18:33:21 -0500

    1.2.0-pre3

--- src/main/kotlin/skytils/skytilsmod/Skytils.kt
+++ src/main/kotlin/skytils/skytilsmod/Skytils.kt
@@ -110,7 +110,7 @@ class Skytils {
     companion object : CoroutineScope {
         const val MODID = "skytils"
         const val MOD_NAME = "Skytils"
-        const val VERSION = "1.2.0-pre2"
+        const val VERSION = "1.2.0-pre3"
 
         @JvmField
         val gson: Gson = GsonBuilder()
