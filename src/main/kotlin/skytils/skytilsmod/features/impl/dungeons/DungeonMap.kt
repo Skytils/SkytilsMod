51302cb67081c16b440034ffa72afe12acc5f96e
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2022-03-01 17:33:39 -0500

    remove use neu from dungeon map

--- src/main/kotlin/skytils/skytilsmod/features/impl/dungeons/DungeonMap.kt
+++ src/main/kotlin/skytils/skytilsmod/features/impl/dungeons/DungeonMap.kt
@@ -25,7 +25,6 @@ import net.minecraft.item.ItemMap
 import net.minecraft.world.storage.MapData
 import net.minecraftforge.event.world.WorldEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
-import org.lwjgl.opengl.GL11
 import skytils.skytilsmod.Skytils
 import skytils.skytilsmod.Skytils.Companion.mc
 import skytils.skytilsmod.core.structure.FloatPair
@@ -33,7 +32,6 @@ import skytils.skytilsmod.core.structure.GuiElement
 import skytils.skytilsmod.utils.SBInfo
 import skytils.skytilsmod.utils.SkyblockIsland
 import skytils.skytilsmod.utils.graphics.SmartFontRenderer
-import skytils.skytilsmod.utils.graphics.colors.CustomColor
 import java.awt.Color
 
 class DungeonMap {
@@ -51,30 +49,11 @@ class DungeonMap {
             if (mapData == null) return
             GlStateManager.color(1f, 1f, 1f, 1f)
             mc.entityRenderer.mapItemRenderer.renderMap(mapData, false)
-            if (!Skytils.usingNEU) {
-                GlStateManager.pushMatrix()
-                val blendEnabled = GL11.glIsEnabled(GL11.GL_BLEND)
-                GlStateManager.enableBlend()
-                GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0)
-                GlStateManager.rotate(45f, 0f, 0f, 1f)
-                fr.drawString(
-                    "USE NEU",
-                    100f,
-                    10f,
-                    alignment = SmartFontRenderer.TextAlignment.MIDDLE,
-                    customColor = CustomColor(50f, 50f, 50f, 0.5f)
-                )
-
-                if (!blendEnabled) GlStateManager.disableBlend()
-                GlStateManager.popMatrix()
-            }
         }
 
         override fun demoRender() {
             Gui.drawRect(0, 0, 128, 128, Color.RED.rgb)
             fr.drawString("Dungeon Map", 64f, 5f, alignment = SmartFontRenderer.TextAlignment.MIDDLE)
-            fr.drawString("Use NEU", 64f, 20f, alignment = SmartFontRenderer.TextAlignment.MIDDLE)
-            fr.drawString("discord.gg/moulberry", 64f, 30f, alignment = SmartFontRenderer.TextAlignment.MIDDLE)
         }
 
         private fun readMapData() {
