70eb2425cd2eaba0d9cd8622d87b8ebf9fd98df3
TakoTheDev <74718793+TakoTheDev@users.noreply.github.com>
2022-03-07 16:27:05 +0100

    Update Config.kt

--- src/main/kotlin/skytils/skytilsmod/core/Config.kt
+++ src/main/kotlin/skytils/skytilsmod/core/Config.kt
@@ -110,6 +110,13 @@ object Config : Vigilant(File("./config/skytils/config.toml"), "Skytils", sortin
         category = "General", subcategory = "Reparty"
     )
     var overrideReparty = true
+    
+    @Property(
+        type = PropertyType.SWITCH, name = "[WIP]Party Finder Stats",
+        description = "Displays Stats about a Player who joined.",
+        category = "General", subcategory = "Party Finder"
+    )
+    var partyFinderStats = true
 
     @Property(
         type = PropertyType.SWITCH, name = "Coop Add Confirmation",
