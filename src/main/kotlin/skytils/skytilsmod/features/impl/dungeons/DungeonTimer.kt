073a51b0391b8e06cac78c5a01a2dca8cd212a14
TakoTheDev <74718793+TakoTheDev@users.noreply.github.com>
2022-03-25 17:17:08 +0100

    again

--- src/main/kotlin/skytils/skytilsmod/features/impl/dungeons/DungeonTimer.kt
+++ src/main/kotlin/skytils/skytilsmod/features/impl/dungeons/DungeonTimer.kt
@@ -63,9 +63,6 @@ class DungeonTimer {
                 )
             }
             message == "§r§c[BOSS] The Watcher§r§f: You have proven yourself. You may pass.§r" -> {
-                if(Skytils.config.rabbitHat) {
-                    GuiManager.createTitle("§c§lRABBIT HAT", 20)
-                }
                 bloodClearTime = System.currentTimeMillis()
                 if (Skytils.config.dungeonTimer) UChat.chat(
                     "§bWatcher took ${diff(bloodClearTime, bloodOpenTime)} seconds to clear."
