1b0511448f2c0f18c66518a33448181c76b5d1dc
TakoTheDev <74718793+TakoTheDev@users.noreply.github.com>
2022-03-25 17:30:57 +0100

    Removed not needed space

--- src/main/kotlin/skytils/skytilsmod/core/Config.kt
+++ src/main/kotlin/skytils/skytilsmod/core/Config.kt
@@ -2600,7 +2600,6 @@ object Config : Vigilant(File("./config/skytils/config.toml"), "Skytils", sortin
         addDependency("changeToSameColorMode", "changeAllSameColorTerminalSolver")
         addDependency("lividFinderType", "findCorrectLivid")
 
-
         listOf(
             "showGriffinCountdown",
             "particleBurrows",
