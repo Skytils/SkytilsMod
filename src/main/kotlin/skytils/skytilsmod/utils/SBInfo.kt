e4b65b9c299487f54b6434177c05faf375292989
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2022-04-03 19:40:11 -0400

    add crimson island to SBIslands

--- src/main/kotlin/skytils/skytilsmod/utils/SBInfo.kt
+++ src/main/kotlin/skytils/skytilsmod/utils/SBInfo.kt
@@ -182,6 +182,9 @@ enum class SkyblockIsland(val formattedName: String, val mode: String) {
     PrivateIsland("Private Island", "dynamic"),
     SpiderDen("Spider's Den", "combat_1"),
     BlazingFortress("Blazing Fortress", "combat_2"),
+
+    //TODO: transfer waypoints from blazing to crimson
+    CrimsonIsle("Crimson Isle", "crimson_isle"),
     TheEnd("The End", "combat_3"),
     GoldMine("Gold Mine", "mining_1"),
     DeepCaverns("Deep Caverns", "mining_2"),
