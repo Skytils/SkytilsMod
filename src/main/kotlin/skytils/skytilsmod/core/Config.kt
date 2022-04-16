36311b64d1ea0d3372bd3f6f78999288733c6c6d
FluxCapacitor <31071265+FluxCapacitor2@users.noreply.github.com>
2022-04-16 11:44:13 -0600

    Highlight door opener (#207)
    * Highlight door opener
    
    * Door opener takes priority over other highlights
    
    * Add dependency because it won't render without spirit leap names enabled (thanks tako)

--- src/main/kotlin/skytils/skytilsmod/core/Config.kt
+++ src/main/kotlin/skytils/skytilsmod/core/Config.kt
@@ -589,6 +589,13 @@ object Config : Vigilant(
     var spiritLeapNames = false
 
     @Property(
+        type = PropertyType.SWITCH, name = "Highlight Door Opener",
+        description = "Highlight the player that most recently opened a Wither Door on the spirit leap menu.",
+        category = "Dungeons", subcategory = "Quality of Life"
+    )
+    var highlightDoorOpener = false
+
+    @Property(
         type = PropertyType.BUTTON, name = "Spirit Leap Highlights",
         description = "Highlights names and classes on the Spirit Leap menu.",
         category = "Dungeons", subcategory = "Quality of Life",
@@ -2647,6 +2654,8 @@ object Config : Vigilant(
         addDependency("messageTitle300Score", "createTitleOn300Score")
 
         addDependency("bloodHelperColor", "bloodHelper")
+
+        addDependency("highlightDoorOpener", "spiritLeapNames")
 
         addDependency("showNextBlaze", "blazeSolver")
         addDependency("lowestBlazeColor", "blazeSolver")
