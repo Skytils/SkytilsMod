6209a817f10472774941994f329bfa732b4498af
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2022-04-24 09:05:52 -0400

    add moodyshot grapplehook or whatever to the prevent placing weapons

--- src/main/kotlin/skytils/skytilsmod/core/Config.kt
+++ src/main/kotlin/skytils/skytilsmod/core/Config.kt
@@ -1292,7 +1292,7 @@ object Config : Vigilant(
 
     @Property(
         type = PropertyType.SWITCH, name = "Prevent Placing Weapons",
-        description = "Stops the game from trying to place the Flower of Truth, Spirit Sceptre, and Weird Tuba items.",
+        description = "Stops the game from trying to place the Flower of Truth, Moody Grappleshot, Spirit Sceptre, and Weird Tuba items.",
         category = "Miscellaneous", subcategory = "Items"
     )
     var preventPlacingWeapons = false
