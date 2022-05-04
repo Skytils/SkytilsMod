d108cba31a2a7917817ecfd92d9f3cba3914fb19
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2022-05-04 14:36:30 -0400

    pray

--- src/main/kotlin/skytils/skytilsmod/utils/APIUtil.kt
+++ src/main/kotlin/skytils/skytilsmod/utils/APIUtil.kt
@@ -25,11 +25,14 @@ import org.apache.hc.client5.http.classic.methods.HttpGet
 import org.apache.hc.client5.http.config.RequestConfig
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder
 import org.apache.hc.client5.http.impl.classic.HttpClients
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactoryBuilder
 import org.apache.hc.core5.http.io.entity.EntityUtils
 import org.apache.hc.core5.http.message.BasicHeader
 import org.apache.hc.core5.util.Timeout
 import skytils.skytilsmod.Skytils
 import skytils.skytilsmod.Skytils.Companion.failPrefix
+import skytils.skytilsmod.tweaker.EssentialPlatformSetup
 import java.awt.image.BufferedImage
 import java.net.HttpURLConnection
 import java.net.URL
@@ -41,6 +44,14 @@ object APIUtil {
 
     val builder: HttpClientBuilder =
         HttpClients.custom().setUserAgent("Skytils/${Skytils.VERSION}")
+            .setConnectionManager(
+                PoolingHttpClientConnectionManagerBuilder.create()
+                    .setSSLSocketFactory(
+                        SSLConnectionSocketFactoryBuilder.create()
+                            .setSslContext(EssentialPlatformSetup.getSSLContext())
+                            .build()
+                    ).build()
+            )
             .setConnectionManagerShared(true)
             .setDefaultHeaders(
                 mutableListOf(
