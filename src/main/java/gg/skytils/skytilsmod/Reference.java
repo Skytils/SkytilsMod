00b489efd135de5fb47d65d6aacc5c449f66a13c
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2022-05-08 02:01:18 -0400

    1.3.0-pre2

--- src/main/java/gg/skytils/skytilsmod/Reference.java
+++ src/main/java/gg/skytils/skytilsmod/Reference.java
@@ -22,5 +22,5 @@ public class Reference {
     public static String dataUrl = "https://data.skytils.gg/";
     public static final String MODID = "skytils";
     public static final String MOD_NAME = "Skytils";
-    public static final String VERSION = "1.3.0-pre1";
+    public static final String VERSION = "1.3.0-pre2";
 }
