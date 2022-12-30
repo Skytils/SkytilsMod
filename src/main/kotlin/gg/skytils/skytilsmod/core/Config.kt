15c762ee94507bcdcada56fd0c68b7e11a25b8c6
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2022-12-30 14:31:15 -0500

    Rename stuff to prevent issues from older versions cause the fix no work

--- src/main/kotlin/gg/skytils/skytilsmod/core/Config.kt
+++ src/main/kotlin/gg/skytils/skytilsmod/core/Config.kt
@@ -197,8 +197,8 @@ object Config : Vigilant(
     var dungeonDeathCounter = false
 
     @Property(
-        type = PropertyType.SWITCH, name = "§b[WIP] §rParty Finder Stats",
-        description = "Displays Stats about a Player who joined.",
+        type = PropertyType.SWITCH, name = "Party Finder Stats",
+        description = "§b[WIP] Displays Stats about a Player who joined.",
         category = "Dungeons", subcategory = "Party Finder"
     )
     var partyFinderStats = false
@@ -1287,7 +1287,7 @@ object Config : Vigilant(
     var disableBlockAnimation = false
 
     @Property(
-        type = PropertyType.DECIMAL_SLIDER, name = "Dropped Item Scale",
+        type = PropertyType.DECIMAL_SLIDER, name = "Dropped Item Size",
         description = "Change the size of dropped items.",
         category = "Miscellaneous", subcategory = "Items",
         maxF = 5f,
@@ -1334,7 +1334,7 @@ object Config : Vigilant(
     var starDisplayType = 0
 
     @Property(
-        type = PropertyType.DECIMAL_SLIDER, name = "Larger Heads",
+        type = PropertyType.DECIMAL_SLIDER, name = "Head Display Size",
         description = "Change the size of heads in your inventory.",
         category = "Miscellaneous", subcategory = "Items",
         maxF = 2f,
@@ -1505,7 +1505,7 @@ object Config : Vigilant(
     var showPetRarity = false
 
     @Property(
-        type = PropertyType.PERCENT_SLIDER, name = "Item Rarity Opacity",
+        type = PropertyType.PERCENT_SLIDER, name = "Item Rarity Transparency",
         description = "How opaque the rarity color will be",
         category = "Miscellaneous", subcategory = "Item Rarity"
     )
@@ -1668,7 +1668,7 @@ object Config : Vigilant(
     var transparentArmorLayer = 1f
 
     @Property(
-        type = PropertyType.PERCENT_SLIDER, name = "Transparent Head Layer",
+        type = PropertyType.PERCENT_SLIDER, name = "Head Layer Transparency",
         description = "Changes the transparency of your head layer.",
         category = "Miscellaneous", subcategory = "Other"
     )
@@ -2837,18 +2837,6 @@ object Config : Vigilant(
             val ver = UpdateChecker.SkytilsVersion(Skytils.config.lastLaunchedVersion)
             when {
                 !ver.isSafe || ver < UpdateChecker.SkytilsVersion("1.2-pre3") || Skytils.config.lastLaunchedVersion == "0" -> {
-                    if (largerHeadScale > 2) {
-                        largerHeadScale /= 100
-                    }
-                    if (itemDropScale > 5) {
-                        itemDropScale /= 100f
-                    }
-                    if (itemRarityOpacity > 1) {
-                        itemRarityOpacity /= 100f
-                    }
-                    if (transparentHeadLayer > 1) {
-                        transparentHeadLayer /= 100f
-                    }
                     if (GuiManager.GUISCALES["Crystal Hollows Map"] == 0.1f) {
                         GuiManager.GUISCALES["Crystal Hollows Map"] = 1f
                         PersistentSave.markDirty<GuiManager>()
