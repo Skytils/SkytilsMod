e18738e05a2c48bf1feb8033fe10ec8759d8c867
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2023-04-12 13:35:20 -0400

    Add route to get SkyblockIsland data

---
+++ src/main/kotlin/gg/skytils/skytilsmod/localapi/routes/skytilsdata.kt
@@ -0,0 +1,31 @@
+/*
+ * Skytils - Hypixel Skyblock Quality of Life Mod
+ * Copyright (C) 2020-2023 Skytils
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package gg.skytils.skytilsmod.localapi.routes
+
+import gg.skytils.skytilsmod.utils.SkyblockIsland
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Route.registerSkytilsDataRoutes() = apply {
+    route("/data") {
+        get("/islands") {
+            context.respond(SkyblockIsland.values())
+        }
+    }
+}
