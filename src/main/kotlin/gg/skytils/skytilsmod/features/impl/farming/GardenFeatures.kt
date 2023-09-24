cd155b30686c6ee7f3979cd31d2963c6166ff871
AzuredBlue <alvaro9d8d@gmail.com>
2023-09-24 13:23:57 +0200

    Reworked Visitor Notification
    Now works with chat messages and an essentials notification.
    Also pretty sure it didnt work when you went offline

--- src/main/kotlin/gg/skytils/skytilsmod/features/impl/farming/GardenFeatures.kt
+++ src/main/kotlin/gg/skytils/skytilsmod/features/impl/farming/GardenFeatures.kt
@@ -18,9 +18,7 @@
 
 package gg.skytils.skytilsmod.features.impl.farming
 
-import gg.essential.universal.UChat
-import gg.essential.universal.utils.MCClickEventAction
-import gg.essential.universal.wrappers.message.UTextComponent
+import gg.essential.api.EssentialAPI
 import gg.skytils.skytilsmod.Skytils
 import gg.skytils.skytilsmod.Skytils.Companion.mc
 import gg.skytils.skytilsmod.core.TickTask
@@ -28,8 +26,10 @@ import gg.skytils.skytilsmod.utils.*
 import net.minecraft.init.Blocks
 import net.minecraft.util.BlockPos
 import net.minecraft.util.MovingObjectPosition
+import net.minecraftforge.client.event.ClientChatReceivedEvent
 import net.minecraftforge.client.event.DrawBlockHighlightEvent
 import net.minecraftforge.event.world.WorldEvent
+import net.minecraftforge.fml.common.eventhandler.EventPriority
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 object GardenFeatures {
@@ -49,8 +49,27 @@ object GardenFeatures {
     // Only up to 1 visitor can spawn if the player is offline or out of the garden, following the same timer.
     private val visitorCount = Regex("^\\s*§r§b§lVisitors: §r§f\\((?<visitors>\\d+)\\)§r\$")
     private val nextVisitor = Regex("\\s*§r Next Visitor: §r§b(?:(?<min>\\d+)m )?(?<sec>\\d+)s§r")
+    private val newVisitorRegex = Regex("^(.+) has arrived on your Garden!\$")
     private var nextVisitorAt = -1L
     private var lastKnownVisitorCount = 0
+
+    @SubscribeEvent(priority = EventPriority.HIGHEST, receiveCanceled = true)
+    fun onChat(event: ClientChatReceivedEvent) {
+        if (!Utils.inSkyblock) return
+        if (!Skytils.config.visitorNotifications) return
+        val unformatted = event.message.unformattedText.stripControlCodes()
+        if (unformatted.matches(newVisitorRegex)) {
+            EssentialAPI.getNotifications()
+                .push(
+                    "New Visitor!",
+                    unformatted,
+                    3f,
+                    action = {
+                        Skytils.sendMessageQueue.add("/warp garden")
+                    })
+
+        }
+    }
 
     init {
         TickTask(5, repeats = true) {
@@ -62,41 +81,6 @@ object GardenFeatures {
                 }.also {
                     if (it != isCleaningPlot && Skytils.config.gardenPlotCleanupHelper) {
                         mc.renderGlobal.loadRenderers()
-                    }
-                }
-
-                if (inGarden) {
-                    val match = ScoreboardUtil.sidebarLines.firstNotNullOfOrNull { nextVisitor.find(it) }
-                    if (match != null) {
-                        val min = match.groups["min"]?.value?.toIntOrNull() ?: 0
-                        val sec = match.groups["sec"]?.value?.toIntOrNull() ?: 0
-                        nextVisitorAt = System.currentTimeMillis() + (min * 60_000L) + (sec * 1000L)
-                    }
-
-                    lastKnownVisitorCount =
-                        (ScoreboardUtil.sidebarLines.firstNotNullOfOrNull { visitorCount.find(it) }?.groups?.get("visitors")?.value?.toIntOrNull()
-                            ?: 0).also {
-                            if (it > lastKnownVisitorCount && Skytils.config.visitorNotifications) {
-                                UChat.chat("${Skytils.prefix} §b${it} visitors are available on your garden!")
-                            }
-                        }
-                }
-
-                if (nextVisitorAt != -1L) {
-                    // Max number of visitors on your island is 5. However, after the 5th visitor the timer keeps going so there can be a 6th one that spawns after the 5th visitor is gone.
-                    if (lastKnownVisitorCount > 5) {
-                        nextVisitorAt = -1L
-                    } else if (System.currentTimeMillis() >= nextVisitorAt) {
-                        // TODO: 15 seconds is not constant, changes based on unique visitors and when crops are broken, however, it provides a good measure for now with a reasonable difference in time
-                        // -0.1s per crop broken
-                        nextVisitorAt += 15_000L
-                        lastKnownVisitorCount++
-                        UChat.chat(
-                            UTextComponent("${Skytils.prefix} §bA new visitor is available on your garden!").setClick(
-                                MCClickEventAction.RUN_COMMAND,
-                                "/warp garden"
-                            )
-                        )
                     }
                 }
             }
