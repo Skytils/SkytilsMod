317ca2211d06e197ffbbf0754f4eb6f1ce2b3861
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2023-12-17 17:34:44 -0500

    feat: inject code into GuiScreen to set mc variable on init

--- src/main/java/gg/skytils/skytilsmod/mixins/transformers/gui/MixinGuiScreen.java
+++ src/main/java/gg/skytils/skytilsmod/mixins/transformers/gui/MixinGuiScreen.java
@@ -19,11 +19,13 @@
 package gg.skytils.skytilsmod.mixins.transformers.gui;
 
 import gg.skytils.skytilsmod.mixins.hooks.gui.GuiScreenHookKt;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiYesNoCallback;
 import net.minecraft.util.IChatComponent;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -31,6 +33,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(GuiScreen.class)
 public abstract class MixinGuiScreen extends Gui implements GuiYesNoCallback {
+
+    @Shadow public Minecraft mc;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void onInit(CallbackInfo ci) {
+        this.mc = Minecraft.getMinecraft();
+    }
 
     @Inject(method = "sendChatMessage(Ljava/lang/String;Z)V", at = @At("HEAD"), cancellable = true)
     private void onSendChatMessage(String message, boolean addToChat, CallbackInfo ci) {
