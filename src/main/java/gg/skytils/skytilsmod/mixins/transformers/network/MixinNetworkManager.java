d5958994cbd09ff0b2a5dd4492a4e0577ab1bf83
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2024-01-18 21:52:52 -0500

    fix: do not fire receive packet event if singleplayer and is server

--- src/main/java/gg/skytils/skytilsmod/mixins/transformers/network/MixinNetworkManager.java
+++ src/main/java/gg/skytils/skytilsmod/mixins/transformers/network/MixinNetworkManager.java
@@ -21,17 +21,23 @@ package gg.skytils.skytilsmod.mixins.transformers.network;
 import gg.skytils.skytilsmod.mixins.hooks.network.NetworkManagerHookKt;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import net.minecraft.network.EnumPacketDirection;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.Packet;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = NetworkManager.class, priority = 1001)
 public abstract class MixinNetworkManager extends SimpleChannelInboundHandler<Packet<?>> {
+    @Shadow @Final private EnumPacketDirection direction;
+
     @Inject(method = "channelRead0", at = @At("HEAD"), cancellable = true)
     private void onReceivePacket(ChannelHandlerContext context, Packet<?> packet, CallbackInfo ci) {
-        NetworkManagerHookKt.onReceivePacket(context, packet, ci);
+        if (this.direction == EnumPacketDirection.CLIENTBOUND)
+            NetworkManagerHookKt.onReceivePacket(context, packet, ci);
     }
 }
