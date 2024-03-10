60a07ea36caea9451d4ff65f446d3b1b8946394d
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2024-03-10 16:39:11 -0400

    build: Update Essential API to 1.3.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -89,7 +89,7 @@ val shadowMeMod: Configuration by configurations.creating {
 
 dependencies {
     shadowMe("gg.essential:loader-launchwrapper:1.2.2")
-    implementation("gg.essential:essential-1.8.9-forge:14616+g169bd9af6a") {
+    implementation("gg.essential:essential-1.8.9-forge:16425+g3a090c5c88") {
         exclude(module = "asm")
         exclude(module = "asm-commons")
         exclude(module = "asm-tree")
