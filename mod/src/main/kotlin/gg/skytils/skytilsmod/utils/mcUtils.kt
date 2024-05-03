ba4022809205a8ecb147f1d32ff08e4b72dbbd07
sychic <47618543+Sychic@users.noreply.github.com>
2024-05-03 01:09:18 -0400

    chore: preprocess `Skytils.kt`

---
+++ mod/src/main/kotlin/gg/skytils/skytilsmod/utils/mcUtils.kt
@@ -0,0 +1,41 @@
+/*
+ * Skytils - Hypixel Skyblock Quality of Life Mod
+ * Copyright (C) 2020-2024 Skytils
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package gg.skytils.skytilsmod.utils
+
+import net.minecraft.launchwrapper.Launch
+import net.minecraftforge.fml.common.Loader
+
+val isDeobfuscatedEnvironment by lazy {
+    //#if FORGE
+    //#if MC<11400
+    Launch.blackboard.getOrDefault("fml.deobfuscatedEnvironment", false) as Boolean
+    //#else
+    //$$ (System.getenv("target") ?: "").lowercase() == "fmluserdevclient"
+    //#endif
+    //#endif
+}
+
+fun isModLoaded(id: String) =
+    //#if FORGE
+    //#if MC<11400
+    Loader.isModLoaded(id)
+    //#else
+    //$$ FMLLoader.getLoadingModList().getModFileById(id)
+    //#endif
+    //#endif
