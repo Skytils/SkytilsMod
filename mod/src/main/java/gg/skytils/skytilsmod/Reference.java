9ab4171a078cf35a8bb3821d7f3f0a1f2a40ea52
sychic <47618543+Sychic@users.noreply.github.com>
2024-05-03 14:45:38 -0400

    fix: read mcmod.info for version

--- mod/src/main/java/gg/skytils/skytilsmod/Reference.java
+++ mod/src/main/java/gg/skytils/skytilsmod/Reference.java
@@ -18,14 +18,17 @@
 
 package gg.skytils.skytilsmod;
 
-import net.minecraftforge.fml.common.MetadataCollection;
-import net.minecraftforge.fml.common.ModMetadata;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import org.apache.logging.log4j.LogManager;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.HashMap;
+import java.util.Enumeration;
 
 public class Reference {
     public static String dataUrl = "https://data.skytils.gg/";
@@ -36,17 +39,25 @@ public class Reference {
     public static final String UNKNOWN_VERSION = "unknown";
 
     private static String getVersion() {
-        URL url = Skytils.class.getResource("mcmod.info");
-        if (url == null) return UNKNOWN_VERSION;
-        try (InputStream input = url.openStream()) {
-            MetadataCollection metadataCollection = MetadataCollection.from(input, "skytils");
-            ModMetadata metadata = metadataCollection.getMetadataForId("skytils",
-                    new HashMap<String, Object>() {{
-                    put("name", "Skytils");
-                    put("version", UNKNOWN_VERSION);
-                }});
-            return metadata.version;
-        } catch (IOException e) {
+        try {
+            Enumeration<URL> urls = Skytils.class.getClassLoader().getResources("mcmod.info");
+            Gson gson = new Gson();
+            while (urls.hasMoreElements()) {
+                URL url = urls.nextElement();
+                try (InputStream is = url.openStream()) {
+                    JsonArray jsonArray = gson.fromJson(new InputStreamReader(is), JsonArray.class);
+                    for (int i = 0; i < jsonArray.size(); i++) {
+                        JsonObject json = jsonArray.get(i).getAsJsonObject();
+                        if (json.get("modid").getAsString().equals("skytils")) {
+                            String version = json.get("version").getAsString();
+                            return version.isEmpty() ? UNKNOWN_VERSION : version;
+                        }
+                    }
+                }
+            }
+            return UNKNOWN_VERSION;
+        } catch (IOException ioe) {
+            LogManager.getLogger(Reference.class).fatal("Failed while getting Skytils version", ioe);
             return UNKNOWN_VERSION;
         }
     }
