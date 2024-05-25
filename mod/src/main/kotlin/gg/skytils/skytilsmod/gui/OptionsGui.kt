1200c08699476c2ae3b6f984cc6118959ebbd2d3
sychic <47618543+Sychic@users.noreply.github.com>
2024-05-25 16:42:00 -0400

    chore: preprocess options gui
    [no ci]

--- mod/src/main/kotlin/gg/skytils/skytilsmod/gui/OptionsGui.kt
+++ mod/src/main/kotlin/gg/skytils/skytilsmod/gui/OptionsGui.kt
@@ -21,14 +21,12 @@ import gg.essential.api.EssentialAPI
 import gg.essential.elementa.ElementaVersion
 import gg.essential.elementa.WindowScreen
 import gg.essential.elementa.components.UIText
-import gg.essential.elementa.constraints.CenterConstraint
-import gg.essential.elementa.constraints.RainbowColorConstraint
-import gg.essential.elementa.constraints.RelativeConstraint
-import gg.essential.elementa.constraints.SiblingConstraint
+import gg.essential.elementa.constraints.*
 import gg.essential.elementa.constraints.animation.Animations
 import gg.essential.elementa.dsl.*
 import gg.essential.universal.UDesktop
 import gg.essential.universal.UKeyboard
+import gg.essential.universal.USound
 import gg.skytils.skytilsmod.Skytils
 import gg.skytils.skytilsmod.features.impl.dungeons.catlas.core.CatlasConfig
 import gg.skytils.skytilsmod.features.impl.funny.skytilsplus.SkytilsPlus
@@ -43,16 +41,21 @@ import gg.skytils.skytilsmod.utils.openGUI
 import gg.skytils.skytilsmod.utils.toStringIfTrue
 import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.GuiScreen
+import net.minecraft.util.ResourceLocation
 import java.net.URI
 
+//#if MC>11400
+//$$ import net.minecraft.sound.SoundEvents
+//#endif
+
 class OptionsGui(val parent: GuiScreen? = null) :
-    WindowScreen(ElementaVersion.V2, newGuiScale = EssentialAPI.getGuiUtil().getGuiScale()) {
+    WindowScreen(ElementaVersion.V2) {
 
     private val skytilsText: UIText =
         UIText(if (Utils.isBSMod) "BSMod${"+".toStringIfTrue(SkytilsPlus.redeemed)}" else "Skytils", shadow = false).childOf(window).constrain {
             x = CenterConstraint()
             y = RelativeConstraint(0.075f)
-            textScale = basicTextScaleConstraint { window.getHeight() / 40 }
+            textScale = RelativeWindowConstraint(0.025f)
         }
 
     private var orderIndex = 0
@@ -72,7 +75,7 @@ class OptionsGui(val parent: GuiScreen? = null) :
             width = 200.pixels()
             height = 20.pixels()
         }.onMouseClick {
-            mc.displayGuiScreen(CommandAliasesGui())
+            displayScreen(CommandAliasesGui())
         }
         SimpleButton("Edit Locations").childOf(window).constrain {
             x = CenterConstraint()
@@ -80,7 +83,7 @@ class OptionsGui(val parent: GuiScreen? = null) :
             width = 200.pixels()
             height = 20.pixels()
         }.onMouseClick {
-            mc.displayGuiScreen(
+            displayScreen(
                 if (it.mouseButton == 1) ElementaEditingGui()
                 else VanillaEditingGui()
             )
@@ -91,7 +94,7 @@ class OptionsGui(val parent: GuiScreen? = null) :
             width = 200.pixels()
             height = 20.pixels()
         }.onMouseClick {
-            mc.displayGuiScreen(KeyShortcutsGui())
+            displayScreen(KeyShortcutsGui())
         }
         SimpleButton("Edit Spam Filters").childOf(window).constrain {
             x = CenterConstraint()
@@ -99,7 +102,7 @@ class OptionsGui(val parent: GuiScreen? = null) :
             width = 200.pixels()
             height = 20.pixels()
         }.onMouseClick {
-            mc.displayGuiScreen(SpamHiderGui())
+            displayScreen(SpamHiderGui())
         }
         SimpleButton("Edit Waypoints").childOf(window).constrain {
             x = CenterConstraint()
@@ -107,7 +110,7 @@ class OptionsGui(val parent: GuiScreen? = null) :
             width = 200.pixels()
             height = 20.pixels()
         }.onMouseClick {
-            mc.displayGuiScreen(WaypointsGui())
+            displayScreen(WaypointsGui())
         }
         SimpleButton("Edit Notifications").childOf(window).constrain {
             x = CenterConstraint()
@@ -115,7 +118,7 @@ class OptionsGui(val parent: GuiScreen? = null) :
             width = 200.pixels()
             height = 20.pixels()
         }.onMouseClick {
-            mc.displayGuiScreen(CustomNotificationsGui())
+            displayScreen(CustomNotificationsGui())
         }
         SimpleButton("Edit Enchantment Names").childOf(window).constrain {
             x = CenterConstraint()
@@ -123,7 +126,7 @@ class OptionsGui(val parent: GuiScreen? = null) :
             width = 200.pixels()
             height = 20.pixels()
         }.onMouseClick {
-            mc.displayGuiScreen(EnchantNamesGui())
+            displayScreen(EnchantNamesGui())
         }
         SimpleButton("Edit Catlas").childOf(window).constrain {
             x = CenterConstraint()
@@ -131,7 +134,7 @@ class OptionsGui(val parent: GuiScreen? = null) :
             width = 200.pixels()
             height = 20.pixels()
         }.onMouseClick {
-            mc.displayGuiScreen(CatlasConfig.gui())
+            displayScreen(CatlasConfig.gui())
         }
         SimpleButton("Open Config Folder").childOf(window).constrain {
             x = CenterConstraint()
@@ -175,7 +178,7 @@ class OptionsGui(val parent: GuiScreen? = null) :
             width = RelativeConstraint(0.1f)
             height = RelativeConstraint(0.05f)
         }.onMouseClick {
-            mc.displayGuiScreen(LegalGui())
+            displayScreen(LegalGui())
         }
         SimpleButton("ur a wizard Harry", true, true).childOf(window).constrain {
             x = 3.pixels
@@ -200,7 +203,11 @@ class OptionsGui(val parent: GuiScreen? = null) :
         if (orderIndex == order.size) {
             orderIndex = 0
             Skytils.displayScreen = SuperSecretGui()
-            mc.theWorld.playAuxSFXAtEntity(mc.thePlayer, 1003, mc.thePlayer.position, 0)
+            //#if MC<11400
+            USound.playSoundStatic(ResourceLocation("random.door_open"), 1f, 1f)
+            //#else
+            //$$ USound.playSoundStatic(SoundEvents.BLOCK_WOODEN_DOOR_OPEN, 1f, 1f)
+            //#endif
         }
     }
 
@@ -213,13 +220,17 @@ class OptionsGui(val parent: GuiScreen? = null) :
         }
     }
 
+    //#if MC<11400
     override fun setWorldAndResolution(mc: Minecraft, width: Int, height: Int) {
         window.onWindowResize()
-        skytilsText.constrain {
-            textScale = basicTextScaleConstraint { window.getHeight() / 40 }
-        }
         super.setWorldAndResolution(mc, width, height)
     }
+    //#else
+    //$$ override fun resize(mc: MinecraftClient, width: Int, height: Int) {
+    //$$     window.onWindowResize()
+    //$$     super.resize(mc, width, height)
+    //$$ }
+    //#endif
 
     companion object {
         private val order = arrayOf(
