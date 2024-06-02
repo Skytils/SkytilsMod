f5f6ea91a4192600c0516106bba100c6b2c0f81e
sychic <47618543+Sychic@users.noreply.github.com>
2024-06-02 01:18:00 -0400

    feat: render world post event
    [no ci]

---
+++ events/src/main/java/gg/skytils/event/mixins/render/MixinGameRenderer.java
@@ -0,0 +1,35 @@
+/*
+ * Skytils - Hypixel Skyblock Quality of Life Mod
+ * Copyright (C) 2020-2024 Skytils
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package gg.skytils.event.mixins.render;
+
+import gg.skytils.event.EventsKt;
+import gg.skytils.event.impl.render.RenderWorldPostEvent;
+import net.minecraft.client.renderer.EntityRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EntityRenderer.class)
+public class MixinGameRenderer {
+    @Inject(method = "renderWorldPass", at = @At(value = "CONSTANT", args = "stringValue=hand"))
+    public void renderWorld(int pass, float partialTicks, long finishTimeNano, CallbackInfo ci) {
+        EventsKt.postSync(new RenderWorldPostEvent(partialTicks));
+    }
+}
