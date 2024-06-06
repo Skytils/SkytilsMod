8a9aefb819f74c6ddb72163dca2e1d87f396cf02
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2024-06-05 20:33:13 -0400

    feat: add BlockStateUpdateEvent
    [no ci]

---
+++ events/src/main/java/gg/skytils/event/mixins/world/MixinChunk.java
@@ -0,0 +1,43 @@
+/*
+ * Skytils - Hypixel Skyblock Quality of Life Mod
+ * Copyright (C) 2020-2024 Skytils
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package gg.skytils.event.mixins.world;
+
+import gg.skytils.event.EventsKt;
+import gg.skytils.event.impl.world.BlockStateUpdateEvent;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.chunk.Chunk;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Chunk.class)
+public abstract class MixinChunk {
+    @Shadow public abstract IBlockState getBlockState(BlockPos pos);
+
+    @Inject(method = "setBlockState", at = @At("HEAD"))
+    private void onBlockChange(BlockPos pos, IBlockState state, CallbackInfoReturnable<IBlockState> cir) {
+        IBlockState old = this.getBlockState(pos);
+        if (old != state) {
+            EventsKt.postSync(new BlockStateUpdateEvent(pos, old, state));
+        }
+    }
+}
