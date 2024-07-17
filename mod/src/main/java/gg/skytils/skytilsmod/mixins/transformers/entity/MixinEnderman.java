7a5415a459c1b33acf144f33ad6ff157a4fe70bc
sychic <47618543+Sychic@users.noreply.github.com>
2024-07-17 15:45:47 -0700

    refactor: migrate disable enderman teleport to a mixin and don't cancel ender pearl teleports
    [no ci]

---
+++ mod/src/main/java/gg/skytils/skytilsmod/mixins/transformers/entity/MixinEnderman.java
@@ -0,0 +1,37 @@
+/*
+ * Skytils - Hypixel Skyblock Quality of Life Mod
+ * Copyright (C) 2020-2024 Skytils
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package gg.skytils.skytilsmod.mixins.transformers.entity;
+
+import gg.skytils.skytilsmod.Skytils;
+import gg.skytils.skytilsmod.utils.Utils;
+import net.minecraft.entity.monster.EntityEnderman;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(EntityEnderman.class)
+public class MixinEnderman {
+    @Inject(method = "teleportTo", at = @At("HEAD"), cancellable = true)
+    private void cancelTeleport(CallbackInfoReturnable<Boolean> cir) {
+        if (Utils.INSTANCE.getInSkyblock() && Skytils.getConfig().getDisableEndermanTeleport()) {
+            cir.setReturnValue(false);
+        }
+    }
+}
