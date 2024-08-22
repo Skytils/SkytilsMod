07a28ce23e68dc52a818af063b437556d0c7bcc0
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2024-08-22 17:19:15 -0400

    fix: bump mixinlayercape priority to 1010

--- src/main/java/gg/skytils/skytilsmod/mixins/transformers/renderer/MixinLayerCape.java
+++ src/main/java/gg/skytils/skytilsmod/mixins/transformers/renderer/MixinLayerCape.java
@@ -30,7 +30,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(LayerCape.class)
+@Mixin(value = LayerCape.class, priority = 1010)
 public abstract class MixinLayerCape implements LayerRenderer<AbstractClientPlayer> {
     @Shadow
     @Final
