07a132fc2fc5b4c5101f374c1ad0dd842f6f5dbf
igoby <49807567+igoby@users.noreply.github.com>
2024-08-28 03:51:34 +0800

    fix: change tick delay from 20 -> 100 for DungeonListener tab list scraping (#496)

--- src/main/kotlin/gg/skytils/skytilsmod/listeners/DungeonListener.kt
+++ src/main/kotlin/gg/skytils/skytilsmod/listeners/DungeonListener.kt
@@ -295,7 +295,7 @@ object DungeonListener {
             }
         }
         tickTimer(2, repeats = true) {
-            if (Utils.inDungeons && mc.thePlayer != null && mc.thePlayer.ticksExisted >= 20 && (DungeonTimer.scoreShownAt == -1L || System.currentTimeMillis() - DungeonTimer.scoreShownAt < 1500)) {
+            if (Utils.inDungeons && mc.thePlayer != null && mc.thePlayer.ticksExisted >= 100 && (DungeonTimer.scoreShownAt == -1L || System.currentTimeMillis() - DungeonTimer.scoreShownAt < 1500)) {
                 val tabEntries = TabListUtils.tabEntries
                 var partyCount: Int? = null
                 if (tabEntries.isNotEmpty() && tabEntries[0].second.contains("§r§b§lParty §r§f(")) {
