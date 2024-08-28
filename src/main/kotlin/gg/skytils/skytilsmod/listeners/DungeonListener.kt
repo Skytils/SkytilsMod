Skytils/SkytilsMod

commit 694f9b7d4111b3d399584dcb08bf05636c51d7bc

fix: change WS coroutine context

src/main/kotlin/gg/skytils/skytilsmod/listeners/DungeonListener.kt
@@ -143,17 +143,18 @@ object DungeonListener {
                     DungeonFeatures.DungeonSecretDisplay.secrets = sec
                     DungeonFeatures.DungeonSecretDisplay.maxSecrets = max
 
-                    IO.launch {
+                    setFoundSecrets@ {
                         val tile = ScanUtils.getRoomFromPos(mc.thePlayer.position)
                         if (tile is Room && tile.data.name != "Unknown") {
-                            val room = DungeonInfo.uniqueRooms.find { tile in it.tiles } ?: return@launch
+                            val room = tile.uniqueRoom ?: return@setFoundSecrets
                             if (room.foundSecrets != sec) {
                                 room.foundSecrets = sec
                                 if (team.size > 1)
-                                    WSClient.sendPacket(C2SPacketDungeonRoomSecret(SBInfo.server ?: return@launch, room.mainRoom.data.name, sec))
+                                    WSClient.sendPacketAsync(C2SPacketDungeonRoomSecret(SBInfo.server ?: return@setFoundSecrets, room.mainRoom.data.name, sec))
                             }
                         }
                     }
+
                 }.ifNull {
                     DungeonFeatures.DungeonSecretDisplay.secrets = -1
                     DungeonFeatures.DungeonSecretDisplay.maxSecrets = -1
@@ -163,8 +164,8 @@ object DungeonListener {
                         .trim() == "> EXTRA STATS <"
                 ) {
                     if (team.size > 1) {
-                        IO.launch {
-                            WSClient.sendPacket(C2SPacketDungeonEnd(SBInfo.server ?: return@launch))
+                        SBInfo.server?.let {
+                            WSClient.sendPacketAsync(C2SPacketDungeonEnd(it))
                         }
                     }
                     if (Skytils.config.dungeonDeathCounter) {
@@ -204,7 +205,7 @@ object DungeonListener {
                     SpiritLeap.doorOpener = null
                     DungeonInfo.keys--
                 } else if (text == "§r§aStarting in 1 second.§r") {
-                    IO.launch {
+                    Skytils.launch {
                         delay(2000)
                         if (DungeonTimer.dungeonStartTime != -1L && team.size > 1) {
                             val party = async {
@@ -212,17 +213,19 @@ object DungeonListener {
                             }
                             val partyMembers = party.await().members.ifEmpty { setOf(mc.thePlayer.uniqueID) }.mapTo(hashSetOf()) { it.toString() }
                             val entrance = DungeonInfo.uniqueRooms.first { it.mainRoom.data.type == RoomType.ENTRANCE }
-                            WSClient.sendPacket(C2SPacketDungeonStart(
-                                serverId = SBInfo.server ?: return@launch,
-                                floor = DungeonFeatures.dungeonFloor!!,
-                                members = partyMembers,
-                                startTime = DungeonTimer.dungeonStartTime,
-                                entranceLoc = entrance.mainRoom.z * entrance.mainRoom.x
-                            ))
-                            while (DungeonTimer.dungeonStartTime != -1L) {
-                                while (outboundRoomQueue.isNotEmpty()) {
-                                    val packet = outboundRoomQueue.poll() ?: continue
-                                    WSClient.sendPacket(packet)
+                            async(WSClient.wsClient.coroutineContext) {
+                                WSClient.sendPacketAsync(C2SPacketDungeonStart(
+                                    serverId = SBInfo.server ?: return@async,
+                                    floor = DungeonFeatures.dungeonFloor!!,
+                                    members = partyMembers,
+                                    startTime = DungeonTimer.dungeonStartTime,
+                                    entranceLoc = entrance.mainRoom.z * entrance.mainRoom.x
+                                ))
+                                while (DungeonTimer.dungeonStartTime != -1L) {
+                                    while (outboundRoomQueue.isNotEmpty()) {
+                                        val packet = outboundRoomQueue.poll() ?: continue
+                                        WSClient.sendPacketAsync(packet)
+                                    }
                                 }
                             }
                         }

src/main/kotlin/gg/skytils/skytilsmod/Skytils.kt
@@ -546,9 +546,7 @@ class Skytils {
             TrophyFish.loadFromApi()
         }
 
-        IO.launch {
-            WSClient.openConnection()
-        }
+        WSClient.openConnection()
     }
 
     @SubscribeEvent
@@ -604,9 +602,7 @@ class Skytils {
         Utils.skyblock = false
         Utils.dungeons = false
 
-        IO.launch {
-            WSClient.closeConnection()
-        }
+        WSClient.closeConnection()
     }
 
     @SubscribeEvent

src/main/kotlin/gg/skytils/skytilsmod/features/impl/dungeons/catlas/handlers/DungeonScanner.kt
@@ -79,8 +79,8 @@ object DungeonScanner {
                 scanRoom(xPos, zPos, z, x)?.let {
                     DungeonInfo.dungeonList[z * 11 + x] = it
                     if (it is Room && it.data.name != "Unknown") {
-                        IO.launch {
-                            DungeonListener.outboundRoomQueue.add(C2SPacketDungeonRoom(SBInfo.server ?: return@launch, it.data.name, xPos, zPos, x, z, it.core, it.isSeparator))
+                        SBInfo.server?.let { server ->
+                            DungeonListener.outboundRoomQueue.add(C2SPacketDungeonRoom(server, it.data.name, xPos, zPos, x, z, it.core, it.isSeparator))
                         }
                     }
                 }

src/main/kotlin/gg/skytils/skytilsmod/features/impl/mining/CHWaypoints.kt
@@ -274,7 +274,7 @@ object CHWaypoints {
 
             fun sendThroughWS() {
                 if (loc.exists()) {
-                    Skytils.IO.launch {
+                    WSClient.wsClient.launch {
                         WSClient.sendPacket(C2SPacketCHWaypoint(serverId = SBInfo.server ?: "", serverTime = mc.theWorld.worldTime, packetType, loc.locX!!.toInt(), loc.locY!!.toInt(), loc.locZ!!.toInt()))
                     }
                 }

src/main/kotlin/gg/skytils/skytilsws/client/WSClient.kt
@@ -28,6 +28,8 @@ import io.ktor.client.plugins.*
 import io.ktor.client.plugins.websocket.*
 import io.ktor.serialization.kotlinx.*
 import io.ktor.websocket.*
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.channels.ClosedReceiveChannelException
 import kotlinx.serialization.ExperimentalSerializationApi
 import java.util.zip.Deflater
@@ -64,34 +66,39 @@ object WSClient {
         }
     }
 
-    suspend fun openConnection() {
+    fun openConnection(): Job {
         if (session != null) error("Session already open")
 
-        wsClient.webSocketSession(System.getProperty("skytils.websocketURL", "wss://ws.skytils.gg/ws")).apply {
-            session = this
-            try {
-                sendSerialized<Packet>(C2SPacketConnect(SkytilsWS.version, Skytils.VERSION))
-                while (true) {
-                    val packet = receiveDeserialized<Packet>()
-                    PacketHandler.processPacket(this@apply, packet)
+        return wsClient.launch {
+            wsClient.webSocketSession(System.getProperty("skytils.websocketURL", "wss://ws.skytils.gg/ws")).apply {
+                session = this
+                try {
+                    sendSerialized<Packet>(C2SPacketConnect(SkytilsWS.version, Skytils.VERSION))
+                    while (true) {
+                        val packet = receiveDeserialized<Packet>()
+                        PacketHandler.processPacket(this@apply, packet)
+                    }
+                } catch(e: ClosedReceiveChannelException) {
+                    e.printStackTrace()
+                    closeExceptionally(e)
+                } catch (e: Throwable) {
+                    e.printStackTrace()
+                    closeExceptionally(e)
+                } finally {
+                    session = null
                 }
-            } catch(e: ClosedReceiveChannelException) {
-                e.printStackTrace()
-                closeExceptionally(e)
-            } catch (e: Throwable) {
-                e.printStackTrace()
-                closeExceptionally(e)
-            } finally {
-                session = null
             }
         }
+
     }
 
-    suspend fun closeConnection() {
+    fun closeConnection() = wsClient.launch {
         session?.close(CloseReason(CloseReason.Codes.NORMAL, "Client closed connection"))
     }
 
     suspend fun sendPacket(packet: Packet) {
         session?.sendSerialized(packet) ?: error("Tried to send packet but session was null")
     }
+
+    fun sendPacketAsync(packet: Packet) = wsClient.launch { sendPacket(packet) }
 }

commit 7ca6d6b2569c8a562ff6a31da913828e1ccdc3a7

fix: regression causing dungeon room found secrets to never be set
This commit fixes a regression introduced in 694f9b7d4111b3d399584dcb08bf05636c51d7bc.

src/main/kotlin/gg/skytils/skytilsmod/listeners/DungeonListener.kt
@@ -143,7 +143,7 @@ object DungeonListener {
                     DungeonFeatures.DungeonSecretDisplay.secrets = sec
                     DungeonFeatures.DungeonSecretDisplay.maxSecrets = max
 
-                    setFoundSecrets@ {
+                    run setFoundSecrets@ {
                         val tile = ScanUtils.getRoomFromPos(mc.thePlayer.position)
                         if (tile is Room && tile.data.name != "Unknown") {
                             val room = tile.uniqueRoom ?: return@setFoundSecrets