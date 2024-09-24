9b0acf11878ace4919d179a0e5a43be43ad85547
TakoTheDev <74718793+TakoTheDev@users.noreply.github.com>
2024-09-24 18:00:49 +0200

    I think this fixes it but idk

--- src/main/java/gg/skytils/skytilsmod/tweaker/EssentialPlatformSetup.java
+++ src/main/java/gg/skytils/skytilsmod/tweaker/EssentialPlatformSetup.java
@@ -32,7 +32,7 @@ import java.net.URI;
 import static gg.skytils.skytilsmod.tweaker.TweakerUtil.*;
 
 public class EssentialPlatformSetup {
-    private static final String[] dataURLCandidates = {System.getProperty("skytils.dataURL"), Reference.dataUrl, "https://skytilsmod-data.pages.dev/", "https://cdn.jsdelivr.net/gh/Skytils/SkytilsMod-Data@main/", "https://mirror.ghproxy.com/https://raw.githubusercontent.com/Skytils/SkytilsMod-Data/main/"};
+    private static final String[] dataURLCandidates = {"https://cdn.jsdelivr.net/gh/TakoTheDev/SkytilsMod-Data@main/", System.getProperty("skytils.dataURL"), Reference.dataUrl, "https://skytilsmod-data.pages.dev/", "https://cdn.jsdelivr.net/gh/Skytils/SkytilsMod-Data@main/", "https://mirror.ghproxy.com/https://raw.githubusercontent.com/Skytils/SkytilsMod-Data/main/"};
 
     private static boolean trySetDataUrl(String url) {
         if (url == null) return false;
