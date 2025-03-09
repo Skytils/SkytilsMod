aa4286c2b8b0533b96bd785a5537361978dfdbd4
songreaver <87545636+Kemmisch@users.noreply.github.com>
2025-03-09 08:19:04 +0000

    Update MapUpdater.kt
    fix null
    
    Signed-off-by: songreaver  <87545636+Kemmisch@users.noreply.github.com>

--- src/main/kotlin/gg/skytils/skytilsmod/features/impl/dungeons/catlas/handlers/MapUpdater.kt
+++ src/main/kotlin/gg/skytils/skytilsmod/features/impl/dungeons/catlas/handlers/MapUpdater.kt
@@ -76,7 +76,7 @@ object MapUpdater {
                 if (mapTile.state.ordinal < room.state.ordinal) {
                     room.state = mapTile.state
                     if (room is Room) {
-                        room.uniqueRoom.state = mapTile.state
+                        room.uniqueRoom?.state = mapTile.state
                         if (room.state == RoomState.GREEN) {
                             room.uniqueRoom?.foundSecrets = room.uniqueRoom?.foundSecrets?.coerceAtLeast(room.data.secrets)
                         }
