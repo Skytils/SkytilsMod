6bb8e1ac5e49fb6de9fd9b98b5798e68049a2ad6
songreaver <87545636+Kemmisch@users.noreply.github.com>
2025-03-09 08:15:23 +0000

    Update src/main/kotlin/gg/skytils/skytilsmod/features/impl/dungeons/catlas/handlers/MapUpdater.kt
    Co-authored-by: My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
    Signed-off-by: songreaver  <87545636+Kemmisch@users.noreply.github.com>

--- src/main/kotlin/gg/skytils/skytilsmod/features/impl/dungeons/catlas/handlers/MapUpdater.kt
+++ src/main/kotlin/gg/skytils/skytilsmod/features/impl/dungeons/catlas/handlers/MapUpdater.kt
@@ -76,7 +76,7 @@ object MapUpdater {
                 if (mapTile.state.ordinal < room.state.ordinal) {
                     room.state = mapTile.state
                     if (room is Room) {
-                        DungeonInfo.uniqueRooms.find {it.name == room.data.name}?.state = mapTile.state
+                        room.uniqueRoom.state = mapTile.state
                         if (room.state == RoomState.GREEN) {
                             room.uniqueRoom?.foundSecrets = room.uniqueRoom?.foundSecrets?.coerceAtLeast(room.data.secrets)
                         }
