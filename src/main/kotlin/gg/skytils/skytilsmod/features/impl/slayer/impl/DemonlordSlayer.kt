065f58b1aafbe23ef2c685c7288a11f192b00a70
songreaver <87545636+Kemmisch@users.noreply.github.com>
2025-03-21 22:21:25 +0000

    fix npe
    totempos is nullable
    
    Signed-off-by: songreaver  <87545636+Kemmisch@users.noreply.github.com>

--- src/main/kotlin/gg/skytils/skytilsmod/features/impl/slayer/impl/DemonlordSlayer.kt
+++ src/main/kotlin/gg/skytils/skytilsmod/features/impl/slayer/impl/DemonlordSlayer.kt
@@ -193,7 +193,7 @@ class DemonlordSlayer(entity: EntityBlaze) :
                     )
                     thrownEntity = e
                     return@tickTimer
-                } else if (e.name.matches(SlayerFeatures.totemRegex) && e.getDistanceSq(totemPos) < 9) {
+                } else if (e.name.matches(SlayerFeatures.totemRegex) && e.getDistanceSq(totemPos ?: return@tickTimer) < 9) {
                     totemEntity = e
                 }
             }
