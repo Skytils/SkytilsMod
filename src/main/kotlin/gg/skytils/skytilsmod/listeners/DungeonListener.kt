Skytils/SkytilsMod

commit 44d836d4ad2ae98f45a5a8fbc29e6557ade826a2

fix(Catlas): Don't render players on different boss map layers

src/main/kotlin/gg/skytils/skytilsmod/listeners/DungeonListener.kt
@@ -356,7 +356,7 @@ object DungeonListener {
 
                     teammate.player = mc.theWorld.playerEntities.find {
                         it.name == teammate.playerName && it.uniqueID.version() == 4
-                    }
+                    } as AbstractClientPlayer
 
                     old?.locationSkin?.let { teammate.skin = it }
 
@@ -396,7 +396,7 @@ object DungeonListener {
     fun onJoinWorld(event: EntityJoinWorldEvent) {
         // S0CPacketSpawnPlayer
         if (event.entity is AbstractClientPlayer && event.entity.uniqueID.version() == 4) {
-            team[event.entity.name]?.player = event.entity as EntityPlayer
+            team[event.entity.name]?.player = event.entity as AbstractClientPlayer
         }
     }
 
@@ -487,7 +487,7 @@ object DungeonListener {
         var tabEntryIndex: Int,
         var skin: ResourceLocation
     ) {
-        var player: EntityPlayer? = null
+        var player: AbstractClientPlayer? = null
             set(value) {
                 field = value
                 if (value != null) {

src/main/kotlin/gg/skytils/skytilsmod/features/impl/dungeons/catlas/core/boss/icons/PlayerIconRenderer.kt
@@ -29,6 +29,8 @@ object PlayerIconRenderer : IconRenderer() {
         DungeonListener.team.forEach { (name, teammate) ->
             val player = teammate.player ?: return@forEach
             if (!teammate.dead || teammate.mapPlayer.isOurMarker) {
+                val layer = iconCtx.map.getLayer(player)
+                if (layer != iconCtx.layer) return@forEach
                 GlStateManager.pushMatrix()
                 GlStateManager.translate(
                     iconCtx.worldToIconX(player.posX.toInt()),

commit 5e0703a2608cdf89962c1057a03586d368bef8fd

fix: null cannot be cast to non-null type AbstractClientPlayer
Fixes regression introduced in commit 44d836d at dev branch (not present in any release yet)

Signed-off-by: Mustafa ÖNCEL <mustafaoncel92@gmail.com>

src/main/kotlin/gg/skytils/skytilsmod/listeners/DungeonListener.kt
@@ -356,7 +356,7 @@ object DungeonListener {
 
                     teammate.player = mc.theWorld.playerEntities.find {
                         it.name == teammate.playerName && it.uniqueID.version() == 4
-                    } as AbstractClientPlayer
+                    } as AbstractClientPlayer?
 
                     old?.locationSkin?.let { teammate.skin = it }
 