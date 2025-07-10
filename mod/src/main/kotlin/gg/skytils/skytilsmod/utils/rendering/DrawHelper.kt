43ddab6ffb499036ea781b9495940ce748218cec
ninjaguardian <cjfairchild51@gmail.com>
2025-07-09 23:26:31 -0500

    More clarification

--- mod/src/main/kotlin/gg/skytils/skytilsmod/utils/rendering/DrawHelper.kt
+++ mod/src/main/kotlin/gg/skytils/skytilsmod/utils/rendering/DrawHelper.kt
@@ -73,7 +73,12 @@ object DrawHelper {
      */
     fun setupContainerScreenTransformations(matrices: UMatrixStack, aboveItems: Boolean = false) {
         var currentScreen = mc.currentScreen
-        if (currentScreen is PopupScreen) { currentScreen = (currentScreen as? AccessorPopupBackground ?: error("Current PopupScreen does not implement AccessorPopupBackground")).underlyingScreen }
+        if (currentScreen is PopupScreen) {
+            currentScreen = (
+                    currentScreen as? AccessorPopupBackground                                        // Cast to AccessorPopupBackground
+                        ?: error("Current PopupScreen does not implement AccessorPopupBackground")   // Error upon fail
+                    ).underlyingScreen                                                               // Grab backgroundScreen if casting succeeded
+        }
         val screen = currentScreen as? AccessorGuiContainer ?: error("Current ${currentScreen?.javaClass?.simpleName ?: "screen"} does not implement AccessorGuiContainer")
         matrices.translate(screen.guiLeft.toFloat(), screen.guiTop.toFloat(), 0f)
         if (aboveItems) {
