90aae6daa58ab79a3067990cb6338a9380b2f8eb
TakoTheDev <74718793+TakoTheDev@users.noreply.github.com>
2026-02-23 19:53:41 +0100

    chore: removed unused imports

--- src/main/kotlin/gg/skytils/skytilsmod/_event/packet.kt
+++ src/main/kotlin/gg/skytils/skytilsmod/_event/packet.kt
@@ -1,9 +1,7 @@
 package gg.skytils.skytilsmod._event
 
 import gg.skytils.event.CancellableEvent
-import net.minecraft.client.gui.DrawContext
 import net.minecraft.network.packet.Packet
-import net.minecraft.screen.slot.Slot
 
 
 class PacketReceiveEvent<T : Packet<*>>(val packet: T) : CancellableEvent()
